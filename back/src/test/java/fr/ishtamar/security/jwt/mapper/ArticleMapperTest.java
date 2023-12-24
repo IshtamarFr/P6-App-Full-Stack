ff4f77220593c9e3814b9423253c9de6c1f38847
IshtamarFr <ishta@ishtamar.fr>
2023-12-24 12:18:36 +0100

    RELEASE VERSION

--- back/src/test/java/fr/ishtamar/security/jwt/mapper/ArticleMapperTest.java
+++ back/src/test/java/fr/ishtamar/security/jwt/mapper/ArticleMapperTest.java
@@ -9,6 +9,7 @@ import fr.ishtamar.security.jwt.service.impl.UserInfoServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
